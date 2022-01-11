691514a75292c88785231760eaeb4795f064a49c
Jungsoo Lee <lkt6930@gmail.com>
2022-01-11 20:20:19 +0900

    세션처리

--- src/main/java/com/myproject/controller/MainController.java
+++ src/main/java/com/myproject/controller/MainController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
